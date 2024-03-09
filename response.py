60ab68f388e5ee5c825134cbf434a6e1593db87d
a-krupka <142113645+a-krupka@users.noreply.github.com>
2024-03-09 20:23:48 +0100

    Add files via upload
    Updated version with working deflection from Left

--- response.py
+++ response.py
@@ -19,19 +19,24 @@ def sign_pos(num):
 #all_loads =[(24.0, -2.0, 'S', '', '', '', ''), (24.0, 0.0, 'S', '', '', '', ''), (-96.0000000000000, 2.0, 'R', 'a', '', '', ''), (24.0, 3.0, 'S', '', '', '', ''), (92.0, 7.0, 'D', '', 23.0, 5.0, 9.0), (-68.0000000000000, 7.0, 'R', 'b', '', '', ''),]#(0.1, 8.0, 'S', '', '', '', '')]
 #all_loads = [(8.0, 2.0, 'S', '', '', '', ''), (-30.7250000000000, 3.0, 'R', 'a', '', '', ''), (10.0, 11.0, 'S', '', '', '', ''), (37.5, 6.25, 'D', '', 15.0, 5.0, 7.5), (-24.7750000000000, 8.0, 'R', 'b', '', '', '')]
 
-
-#all_loads = [(-3.60000000000000, 0.0, 'R', 'a', '', '', ''), (2.0, 2.0, 'S', '', '', '', ''), (6.0, 3.0, 'S', '', '', '', ''), (-4.40000000000000, 5.0, 'R', 'b', '', '', '')]
+# Mohrovka, příklad C
+all_loads = [(-3.60000000000000, 0.0, 'R', 'a', '', '', ''), (2.0, 2.0, 'S', '', '', '', ''), (6.0, 3.0, 'S', '', '', '', ''), (-4.40000000000000, 5.0, 'R', 'b', '', '', '')]
 
 #all_loads =[(-1.50000000000000, 0.0, 'R', 'a', '', '', ''), (12.0, 3.5, 'D', '', 4.0, 2.0, 5.0), (-17.5000000000000, 5.0, 'R', 'b', '', '', ''), (7.0, 6.5, 'S', '', '', '', '')]
 #all_loads = [(-35.9500000000000, 0.0, 'R', 'a', '', '', ''), (65.0, 4.5, 'D', '', 13.0, 2.0, 7.0), (2.0, 9.0, 'S', '', '', '', ''), (-31.0500000000000, 10.0, 'R', 'b', '', '', '')]
-all_loads = [(10.0, -1.0, 'S', '', '', '', ''), (20.0, 0.0, 'S', '', '', '', ''), (-87.3333333333333, 0.0, 'R', 'a', '', '', ''), (25.0, 3.0, 'S', '', '', '', ''), (132.0, 6.0, 'D', '', 33.0, 4.0, 8.0), (-179.666666666667, 9.0, 'R', 'b', '', '', ''), (80.0, 9.5, 'S', '', '', '', '')]
+#all_loads = [(10.0, -1.0, 'S', '', '', '', ''), (20.0, 0.0, 'S', '', '', '', ''), (-87.3333333333333, 0.0, 'R', 'a', '', '', ''), (25.0, 3.0, 'S', '', '', '', ''), (132.0, 6.0, 'D', '', 33.0, 4.0, 8.0), (-179.666666666667, 9.0, 'R', 'b', '', '', ''), (80.0, 9.5, 'S', '', '', '', '')]
 #all_loads=[(-5.84615384615385, 0.0, 'R', 'a', '', '', ''), (2.0, 1.0, 'S', '', '', '', ''), (9.0, 3.5, 'D', '', 3.0, 2.0, 5.0), (-5.15384615384615, 6.5, 'R', 'b', '', '', '')]
+
 all_loads = [(16.0, 2.0, 'D', '', 4.0, 0.0, 4.0), (-21.3333333333333, 4.0, 'R', 'a', '', '', ''), (5.33333333333333, 10.0, 'R', 'b', '', '', '')]
+
 #all_loads = [(-12.0000000000000, 0.0, 'R', 'a', '', '', ''), (24.0, 5.0, 'D', '', 4.0, 2.0, 8.0), (-12.0000000000000, 10.0, 'R', 'b', '', '', '')]
-positions = [i[1] for i in all_loads] # gets the position of a load
+
+positions = [i[1] if i[2] != 'D' else i[5] for i in all_loads] # gets the position of a load, in case of distributed load, outputs starting position
+reaction_a = float("".join([str(i[1]) for i in all_loads if i[3] == 'a'])) #computing position of reaction a by list comprehension, then converting into single float
+reaction_b = float("".join([str(i[1]) for i in all_loads if i[3] == 'b'])) #computing position of reaction a by list comprehension, then converting into single float
+print(f"a = {reaction_a} b = {reaction_b}")
 starting_pos = min(positions) # gets the starting (most left) position of force
 t = starting_pos
-
 
 
 print(positions,f"min pos is {min(positions)}")
@@ -42,10 +47,16 @@ for i in all_loads:
         if i[6] > max(positions):
             positions.append(i[6])
 print(positions)
-if t >= 0:
-    a= np.arange(0.001,max(positions) + abs(t) + 0.001,0.001)
-else:                                                               #tohle smazat asi?
-    a = np.arange(0.001, max(positions) + abs(t) + 0.001, 0.001)
+if t <= 0: #tady tweakuju
+    max_pos = max(positions) + abs(t) + 0.001
+    a= np.arange(0.001,max_pos,0.001)
+    odchylka = 0
+    posun = abs(t)
+else:
+    max_pos = max(positions) + 0.001
+    a = np.arange(0.001, max_pos, 0.001)
+    odchylka = abs(t)
+    posun = 0
 Vy = [0.0 for i in a] #Posouvačky # making a list of len(a) of every value 0.0 to which we'll be adding results of for loop
 Vx = [i for i in a]
 Vnun=[0.0 for i in a] # auxillary temporary list which stores results of most recent iteration
@@ -72,9 +83,10 @@ for j in range(len(all_loads)):
 
 Vy = list(map(add, Vy, Vnun)) # adding the last remaining values
 Vy[0] = 0 # rewriting the first value on the y axis as 0 so it starts from zero
+
 xvpoints = np.array(Vx) # making a numpy array from points on the x-axis of shear force diagram
 yvpoints = np.array(Vy) # making a numpy array from points on the y-axis of shear force diagram
-
+print(f"length of x {len(xvpoints)}, length of y {len(yvpoints)}")
 
 
 
@@ -97,13 +109,15 @@ for j in range(len(all_loads)):
             nun.append(result)
 y = list(map(add, y, nun))
 
-for i,j in zip(x,y):
-    print(i,j)
+#for i,j in zip(x,y):
+#    print(i,j)
 xy = [(x[i],y[i]) for i in range(len(y))]
 
 xpoints = np.array(x)
 ypoints = np.array(y)
 mpoints = np.array(xy)
+print(len(xvpoints),len(xpoints),"tady dole")
+print(min(xpoints))
 if abs(min(ypoints)) < abs(max(ypoints)): # computing if the maximal bending moment is positive or negative
     Mmax = max(ypoints)
 else:
@@ -116,7 +130,7 @@ ax1.set_title(r"$Loads$")
 ax1.plot([0+abs(t),0+abs(t)],[min(ypoints-10),max(ypoints)+10],linestyle = "dotted",color = "#bfbfbf")
 ax1.plot([min(xpoints),max(xpoints)],[0,0],linestyle = "dotted",color = "#bfbfbf")
 plt.xticks(np.arange(t, positions[-1]+abs(t), step=1),labels=[i-abs(t) for i in range(round(positions[0]),ceil(positions[-1]+abs(t)))])
-ax1.plot([min(xpoints),max(xpoints)],[0,0],color = "black",alpha=0.8)   #black line on x-axis
+ax1.plot([min(xpoints)+odchylka,max(xpoints)+odchylka],[0,0],color = "black",alpha=0.8)   #black line on x-axis
 for i in all_loads:
     if i[3] == "a":
         ax1.plot(i[1]+abs(t),-abs(0.01*min(ypoints)),marker = "^", color = "r")
@@ -153,7 +167,7 @@ for i in all_loads:
 ax2.set_title(r"$Bending\ moments$")
 ax2.plot(xpoints, ypoints)
 ax2.plot([0+abs(t),0+abs(t)],[min(ypoints-10),max(ypoints)+10],linestyle = "dotted",color = "#bfbfbf")
-ax2.plot([min(xpoints),max(xpoints)],[0,0],color = "black",alpha=0.8)   #black line on x-axis
+ax2.plot([min(xpoints)+odchylka,max(xpoints)+odchylka],[0,0],color = "black",alpha=0.8)   #black line on x-axis
 ax2.plot(x[y.index(min(y))],min(ypoints),marker = "o")
 ax2.plot(x[y.index(max(y))],max(ypoints),marker = "o")
 plt.xticks(np.arange(t, positions[-1]+abs(t), step=1),labels=[i-abs(t) for i in range(round(positions[0]),ceil(positions[-1]+abs(t)))])
@@ -173,30 +187,105 @@ for i in all_loads:
 ax3.set_title(r"$Shear\ forces$")
 ax3.plot(xvpoints,-yvpoints)
 ax3.plot([0+abs(t),0+abs(t)],[min(ypoints-10),max(ypoints)+10],linestyle = "dotted",color = "#bfbfbf")
-ax3.plot([min(xpoints),max(xpoints)],[0,0],color = "black",alpha=0.8)
+ax3.plot([min(xpoints)+odchylka,max(xpoints)+odchylka],[0,0],color = "black",alpha=0.8) # black line on x axis
 plt.xticks(np.arange(t, positions[-1]+abs(t), step=1),labels=[i-abs(t) for i in range(round(positions[0]),ceil(positions[-1]+abs(t)))])
 #ax5 = fig.add_subplot(gs[0:2, 1])
+
 subcentroid=0
 Area=0
-for i in range(0,int((max(positions) + abs(t))*1000)):   #V tomhle může být chyba?
-    subcentroid += (xpoints[i]-0.0005)*ypoints[i]*0.001
-    Area+=ypoints[i]*0.001
-centroid = subcentroid/Area
-#for i in np.arange(0.001,max(positions) + abs(t) + 0.001,0.001):   #V tomhle může být chyba?
-    #index = int(i * 1000)-1
-    #subcentroid += (xpoints[index]-0.0005)*ypoints[index]*0.001*0.001
-    #Area+=ypoints[index]*0.001
-#centroid = subcentroid/Area
+count = 0
+if t < reaction_a:
+    for i in a:
+        if count == reaction_a * 1000:
+            centroid_L = subcentroid / Area
+            Area_L = Area
+            subcentroid = 0
+            Area = 0
+        subcentroid += (xpoints[count] - 0.0005) * ypoints[count]
+        Area += ypoints[count]
+        count += 1
+    centroid_P = subcentroid / Area
+    Area_P = Area
+    Area = Area_P + Area_L # net Area
+    print(f"centroid P = {centroid_P} centroid L = {centroid_L}")
+    print(f"Areas = L -{Area_L} P -{Area_P} ... NET {Area_P + Area_L}")
+    centroid = (Area_P * centroid_P + Area_L * centroid_L) / Area
+elif max(positions) > reaction_b:
+    for i in a:
+        if count == reaction_b * 1000:
+            centroid_L = subcentroid / Area
+            Area_L = Area
+            subcentroid = 0
+            Area = 0
+        subcentroid += (xpoints[count] - 0.0005) * ypoints[count]
+        Area += ypoints[count]
+        count += 1
+    centroid_P = subcentroid / Area
+    Area_P = Area
+    Area = Area_P + Area_L # net Area
+    print(f"centroid P = {centroid_P} centroid L = {centroid_L}")
+    print(f"Areas = L -{Area_L} P -{Area_P} ... NET {Area_P + Area_L}")
+    centroid = (Area_P * centroid_P + Area_L * centroid_L) / Area
+else:
+    for i in a:
+        subcentroid += (xpoints[count]-0.0005)*ypoints[count]
+        Area+=ypoints[count]
+        count += 1
+    centroid = subcentroid/Area
 
-print("centroid =", centroid, "and", "area = ", Area)
-der3 = []
-pokus = int((-((max(positions) + abs(t)) - centroid)*Area/(max(positions) + abs(t)))*1000)
-print("pokus =",pokus)
-for i in range(0,(int((max(positions) + abs(t))*1000))):
-    pokus +=ypoints[i]
-    der3.append(pokus)
+
+if t < reaction_a:
+    der3 = []
+    if reaction_a < centroid_P < reaction_b:
+        cantilever_R = -Area - ((-Area_P * (centroid_P-reaction_a)) / (reaction_b-reaction_a))
+        cantilever_M = (-Area * centroid) -(((-Area_P * (centroid_P-reaction_a)) / (reaction_b-reaction_a))*reaction_b)
+        print("here")
+    else:
+        cantilever_R = -Area
+        cantilever_M = -Area * centroid
+    print(f"cantilever_R = {cantilever_R}, reaction_b = {((-Area_P * (centroid_P-reaction_a)) / (reaction_b-reaction_a))}")
+    count = 0
+    hodnota_pocatku = cantilever_R
+    for i in a:
+        hodnota_pocatku += ypoints[count]
+        der3.append(hodnota_pocatku)
+        count += 1
+elif max(positions) > reaction_b:
+    der3 = []
+    if reaction_a < centroid_L < reaction_b:
+        cantilever_R = -Area - ((-Area_L * (centroid_L - reaction_a)) / (reaction_b - reaction_a))
+        cantilever_M = (-Area * centroid) - (((-Area_L * (centroid_L - reaction_a)) / (reaction_b - reaction_a)) * reaction_b)
+        print("here")
+    else:
+        cantilever_R = -Area
+        cantilever_M = -Area * centroid
+    print(f"cantilever_R = {cantilever_R}, reaction_b = {((-Area_P * (centroid_P - reaction_a)) / (reaction_b - reaction_a))}")
+    count = 0
+    hodnota_pocatku = cantilever_R
+    for i in a:
+        hodnota_pocatku += ypoints[count]
+        der3.append(hodnota_pocatku)
+        count += 1
+
+
+else:
+    print("centroid =", centroid, "and", "area = ", Area)
+    der3 = []
+    pokus = int((-((reaction_b) - centroid)*Area/(reaction_b-reaction_a)))
+    count = 0
+    hodnota_pocatku = 0
+    for i in a:
+        hodnota_pocatku += ypoints[count]
+        der3.append(hodnota_pocatku)
+        if count * 0.001 == reaction_a:
+            hodnota_pocatku += pokus
+            pokus =0
+        count += 1
+
 #print("der3 =", der3)
 der3points = np.array(der3)
+#for i,j in zip(xpoints,der3points):
+#   print(i,j )
 
 ax4 = fig.add_subplot(gs[0, 1])
 ax4.set_title(r"$Slopes\ \phi$")
@@ -208,10 +297,16 @@ ax5 = fig.add_subplot(gs[1, 1])
 ax5.set_title(r"$Deflections$")
 der4 = []
 hokus=0
-for i in range(0,(int((max(positions) + abs(t))*1000))):
-    hokus += der3points[i]*0.001
+#hokus += -cantilever_M
+count = 0
+for i in a:
+    hokus += der3points[count]*0.001
     der4.append(hokus)
+    hokus += -cantilever_M
+    cantilever_M = 0
+    count += 1
 der4points = np.array(der4)
+
 ax5.plot(xpoints,-der4points)
 ax5.plot([0+abs(t),0+abs(t)],[min(der4points-10),max(der4points)+10],linestyle = "dotted",color = "#bfbfbf")
 ax5.plot([min(xpoints),max(xpoints)],[0,0],color = "black",alpha=0.8)
